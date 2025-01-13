9a2dcc57975496d2999fcdb4f9255cfd5e225e92
rob roffey <roffeyre@gmail.com>
2025-01-13 11:14:16 +0000

    associations and validations added to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,11 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+  has_many :categories, through: :bookmarks
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :rating, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 10
+  }, allow_nil: true
 end
