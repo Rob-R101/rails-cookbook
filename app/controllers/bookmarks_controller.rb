043bce32d431e5f6f8679a2e1516c4321388f113
rob roffey <roffeyre@gmail.com>
2025-01-13 15:26:27 +0000

    categories controllers views forms all coded

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,4 +3,16 @@ class BookmarksController < ApplicationController
     @category = Category.find(params[:category_id])
     @bookmark = Bookmark.new
   end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @category
+
+    if @bookmark.save
+      redirect_to categories_path{@category}, notice: "Saved successfully"
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,21 @@ class CategoriesController < ApplicationController
   end
 
   def new
-    @categories = Category.new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
